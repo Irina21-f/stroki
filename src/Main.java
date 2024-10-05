d403f1be35b2d589d5d33c1e92bb40ff9af1663e
ira <ilovekfc45@gmail.com>
2024-10-05 22:38:32 +0500

    вщту

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+//        Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("Ф. И. О. сотрудника — %s\n", fullName);
+//        Задача 2
+        System.out.printf("Данные Ф. И. О. сотрудника для заполнения отчетf — %s\n", fullName.toUpperCase());
+//        Задача 3
+        fullName = "Иванов Семён Семёнович";
+        System.out.printf("Ф. И. О. сотрудника — %s\n", fullName.replace('ё', 'е'));
     }
 }
